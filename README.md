20003497326a66021377309330dcf3a82894ea60
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2022-06-08 18:39:56 -0700

    Mas tecnologías y Readme

---
+++ README.md
@@ -0,0 +1,27 @@
+# Proyecto 1 - Página Web [Clonar una página]
+
+Sistemas y Tecnologías Web - Sección 10
+
+Página a clonar: https://www.nintendo.com/es_LA/
+
+## Tecnologías Utilizadas
+
+- React
+- Babel
+- Webpack
+- Sass
+- Prettier
+- Eslint
+- Css-loader
+
+## Demo
+
+Resultado (Soporta resolución Full HD):
+
+## Screenshots
+
+![App Screenshot]<img src="/show.jpg" alt="Show"/>
+
+## Authors
+
+- Cristian Laynez - 201281
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 // ! Con mi propio webpack.config.js
 export default {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        // publicPath: '/dist/'
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    // publicPath: '/dist/'
+  },
+  devServer: {
+    static: {
+      directory: 'dist',
     },
-    devServer: {
-        static: {
-            directory: 'dist'
-        },
-        compress: true,
-        port: 9000
-    },
-    module: {
-        rules: [
-            { test: [/\.jsx?$/], use: ['babel-loader'] },
-            { test: /\.css$/, use: ["style-loader", "css-loader"] },
-            { test: /\.(png|jpg|gif|avif|jfif)$/, loader: 'url-loader' },
-            { test: [/\.(svg|mp3)$/], use: ['file-loader']},
-        ]
-    }
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      { test: [/\.jsx?$/], use: ['babel-loader'] },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.(png|jpg|gif|avif|jfif)$/, loader: 'url-loader' },
+      { test: [/\.(svg|mp3)$/], use: ['file-loader'] },
+    ],
+  },
 }
